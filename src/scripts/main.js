45472f3d5352094d744253910773dde6a09d8eca
Jack Parsons <jcksnparsons@yahoo.com>
2020-02-19 14:00:58 -0600

    authorization features work for Tasks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,15 @@
 import API from "./tasks/dataHandler.js";
 import DOMrender from "./tasks/DOMrender.js";
-import events from "./tasks/eventListeners.js";
+import taskEvents from "./tasks/eventListeners.js";
+import logInForm from "./auth/logInForm.js"
+import authEvents from "./auth/eventListeners.js"
 
+logInForm.makeLogInForm();
 API.getTasks().then(DOMrender.putTasksOnDom);
-events.renderNewTaskForm();
-events.saveNewTask();
-events.deleteTask();
-events.editTask();
-events.saveEditedTask();
-events.editComplete();
+taskEvents.renderNewTaskForm();
+taskEvents.saveNewTask();
+taskEvents.deleteTask();
+taskEvents.editTask();
+taskEvents.saveEditedTask();
+taskEvents.editComplete();
+authEvents.logIn()
--- src/scripts/tasks/DOMrender.js
+++ src/scripts/tasks/DOMrender.js
@@ -2,12 +2,15 @@ import HTMLFactories from "./HTMLfactory.js";
 
 const DOMrender = {
   putTasksOnDom(tasks) {
+    const activeUser = parseInt(sessionStorage.getItem("userId"));
     const container = document.getElementById("tasks");
     container.innerHTML = "";
 
     tasks.forEach(task => {
-      if (task.isComplete === false) {
-        container.innerHTML += HTMLFactories.makeTaskEntry(task);
+      if (task.userId === activeUser) {
+        if (task.isComplete === false) {
+          container.innerHTML += HTMLFactories.makeTaskEntry(task);
+        }
       }
     });
   }
